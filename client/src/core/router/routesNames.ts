05b41d518dadbb0bedcec71939607541bc930256
olgakup <olga.kupchevskaya.812@my.csun.edu>
2022-05-09 18:26:15 -0700

    feat(client): added navigation drawer

---
+++ client/src/core/router/routesNames.ts
@@ -0,0 +1,74 @@
+interface Route {
+    [key: string]: {
+        NAME: string
+        PATH: string
+    }
+}
+const ROUTE_NAME: Route = {
+    HOME: { NAME: 'Home', PATH: '/' },
+    BLOCKS: {
+        PATH: '/blocks',
+        NAME: 'blocks'
+    },
+    BLOCK_NUMBER: {
+        PATH: '/block/number/:blockRef',
+        NAME: 'block'
+    },
+    BLOCK_HASH: {
+        PATH: '/block/hash/:blockRef',
+        NAME: 'blockHash'
+    },
+    TXS: {
+        PATH: '/txs',
+        NAME: 'transactions'
+    },
+    TX_HASH: {
+        PATH: '/tx/:txRef',
+        NAME: 'transaction'
+    },
+    TXS_PENDING: {
+        PATH: '/pending-txs',
+        NAME: 'pending'
+    },
+    ADDRESS: {
+        PATH: '/address/:addressRef',
+        NAME: 'address'
+    },
+    CHARTS: {
+        PATH: '/charts',
+        NAME: 'charts'
+    },
+    CHART: {
+        PATH: '/chart/:chartRef',
+        NAME: 'chart-detail'
+    },
+    TOKEN: {
+        PATH: '/token/:addressRef',
+        NAME: 'token-detail'
+    },
+    TOKENS: {
+        PATH: '/tokens',
+        NAME: 'tokens'
+    },
+    ABOUT: {
+        PATH: '/about',
+        NAME: 'about'
+    },
+    NOT_FOUND: {
+        PATH: '/404',
+        NAME: 'notFound'
+    },
+    SEARCH_NOT_FOUND: {
+        PATH: '/search/not_found/:searchTerm',
+        NAME: 'search-not-found'
+    },
+    FAV_ADDRESS: {
+        PATH: '/fav_addresses',
+        NAME: 'fav_addresses'
+    },
+    FAV_TOKENS: {
+        PATH: '/tokens/favorites',
+        NAME: 'fav_tokens'
+    }
+}
+export { ROUTE_NAME }
