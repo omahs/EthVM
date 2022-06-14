f73701a05d383c0cf96698a227d076c99805f63e
Abdulqudus <abdulqudusabubakre@gmail.com>
2022-06-14 19:10:57 +0100

    fix: 🐛 remove unused libraries

--- client/vue.config.js
+++ client/vue.config.js
@@ -26,12 +26,7 @@ module.exports = defineConfig({
         resolve: {
             fallback: {
                 http: require.resolve('stream-http'),
-                https: require.resolve('https-browserify'),
-                crypto: require.resolve('crypto-browserify'),
-                stream: require.resolve('stream-browserify'),
-                os: require.resolve('os-browserify/browser'),
-                url: require.resolve('url'),
-                assert: require.resolve('assert')
+                stream: require.resolve('stream-browserify')
             },
             alias: {
                 '@': path.resolve(__dirname, 'src/'),
